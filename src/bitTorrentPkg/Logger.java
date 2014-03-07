3551743d0f7feac131aeefbc1ed14b78ace3d949
JoeyS7 <joeysiracusa@gmail.com>
2014-03-07 16:54:08 -0500

    More Logger methods
    Wrote about half the methods we will need for logging.  We’ll still
    need around five more, and we should consider adding our own.

--- src/bitTorrentPkg/Logger.java
+++ src/bitTorrentPkg/Logger.java
@@ -43,15 +43,49 @@ public class Logger {
 	
 	//methods for writing to log file
 	public void testLog(){
-		writer.println(new Time(System.currentTimeMillis()).toString() + ": Test log. Peer ID = " + peerID);
+		//this log has been tested and works
+		writer.println(new Date().toString() + ": Test log. Peer ID = " + peerID);
 	}
 	
+	//TODO: All logs beneath here have not been tested, with the exception of closeLog()
+	
 	public void logTCPConnection(int targetPeerID){
+		writer.println(new Date().toString() + ": Peer " + peerID + " makes a connection to Peer " 
+						+ targetPeerID);
+	}
+	
+	public void logChangeOfPrefNeighbors(int neighborIDs[]){
+		String IDs = null;
+		for(int i = 0; i < neighborIDs.length; i++){
+			if(i != neighborIDs.length -1) //if it's not the last ID
+				IDs = IDs + neighborIDs[i] + ","; //add a comma
+			else
+				IDs = IDs + neighborIDs[i]; //else no comma
+		
+		}
+		writer.println(new Date().toString() + "Peer " + peerID + " has the preferred neighbors " + IDs);
 		
 	}
 	
+	public void logChangeOfOptUnchokedNeighbor(int neighborID){
+		writer.println(new Date().toString() + "Peer " + peerID + " has optimistically unchoked neighbor "
+						+ neighborID);
+	}
+	
+	public void logUnchoking(int unchokedID){
+		writer.println(new Date().toString() + "Peer " + peerID + " is unchoked by " + unchokedID);
+	}
+	
+	public void logChoking(int chokedID){
+		writer.println(new Date().toString() + "Peer " + peerID + " is choked by " + chokedID);
+	}
+	
+	//TODO: Write methods for logging have messages, interested messages, 
+	//		not interested messages, downloading a piece, and completion of download
+	//		Also consider creating non-required logs for debugging in the future
+	
 	public void closeLog(){
-		writer.println(new Time(System.currentTimeMillis()).toString() + ": Log file closed.");
+		writer.println(new Date().toString() + ": Log file closed.");
 		writer.close();
 	}
 	

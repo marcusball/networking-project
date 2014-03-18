cbdf74491438f12bcf6e93595d1867f297006c48
JoeyS7 <joeysiracusa@gmail.com>
2014-03-17 23:27:39 -0400

    Sending Marcus's Messages
    I implemented methods to write to the socket’s output stream.

--- src/bitTorrentPkg/Edge.java
+++ src/bitTorrentPkg/Edge.java
@@ -1,9 +1,18 @@
 package bitTorrentPkg;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
+
+import bitTorrentPkg.Messages.Bitfield;
+import bitTorrentPkg.Messages.Choke;
+import bitTorrentPkg.Messages.Have;
+import bitTorrentPkg.Messages.Interested;
+import bitTorrentPkg.Messages.NotInterested;
+import bitTorrentPkg.Messages.Request;
+import bitTorrentPkg.Messages.Unchoke;
 
 public class Edge {
 	private Peer origin;
@@ -31,7 +40,7 @@ public class Edge {
 		
 	}
 	
-	public void sendHandshake() {
+	public void sendHandshake() throws IOException {
 		char[] handshake = new char[32];
 		
 		// First five elements in char array is HELLO
@@ -51,6 +60,60 @@ public class Edge {
 			// Last 4 in handshake message 
 			handshake[j] = (char) (origin.getPeerID()%10);
 		}
+		
+		PrintWriter out = new PrintWriter(sender.getOutputStream());
+		out.print(handshake);
+		out.close();
 	}
-
+	
+	public void sendChoke() throws IOException{
+		Choke choke = new Choke();
+		PrintWriter out = new PrintWriter(sender.getOutputStream());
+		out.print(choke.toBytes());
+		out.close();
+	}
+	
+	public void sendUnchoke() throws IOException{
+		Unchoke unchoke = new Unchoke();
+		PrintWriter out = new PrintWriter(sender.getOutputStream());
+		out.print(unchoke.toBytes());
+		out.close();
+	}
+	
+	public void sendInterested() throws IOException{
+		Interested interested = new Interested();
+		PrintWriter out = new PrintWriter(sender.getOutputStream());
+		out.print(interested.toBytes());
+		out.close();
+	}
+	
+	public void sendNotInterested() throws IOException{
+		NotInterested notinterested = new NotInterested();
+		PrintWriter out = new PrintWriter(sender.getOutputStream());
+		out.print(notinterested.toBytes());
+		out.close();
+	}
+	
+	public void sendHave(int index) throws IOException{
+		Have have = new Have(index);
+		PrintWriter out = new PrintWriter(sender.getOutputStream());
+		out.print(have.toBytes());
+		out.close();
+	}
+	
+	public void sendBitfield(byte bitfield[]) throws IOException{
+		Bitfield thebitfield = new Bitfield(bitfield);
+		PrintWriter out = new PrintWriter(sender.getOutputStream());
+		out.print(thebitfield.toBytes());
+		out.close();
+	}
+	
+	public void sendRequest(int index) throws IOException{
+		Request request = new Request(index);
+		PrintWriter out = new PrintWriter(sender.getOutputStream());
+		out.print(request.toBytes());
+		out.close();
+	}
+	
+	
 }
